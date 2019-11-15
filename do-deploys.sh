90cf98f18bfbda081833c3fb31c4e65f51a17d1f
Gene Liverman <gene.liverman+06301990@puppet.com>
2019-11-15 15:53:46 -0500

    Argo CD 1.3.0 via helm, OpenFaaS Operator
    Starting with v1.3.0, the version of kustomize needed by Argo CD no
    longer matches the version included in kubectl. For this reason, I am
    switching from a kustomize-based install to using the helm chart.
    
    This commit also switches OpenFaaS to using its operator.

--- do-deploys.sh
+++ do-deploys.sh
@@ -7,15 +7,15 @@ DIR="$(cd "$(dirname "$0")" && pwd -P)"
 echo 'make storage path'
 mkdir -p /opt/local-path-provisioner
 
+echo 'prep helm'
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+
 echo 'install Argo CD'
-kubectl create namespace argocd
-kubectl kustomize $DIR/configs/argocd |kubectl apply -f -
+helm dependency update $DIR/configs/argocd
+helm upgrade argocd --install $DIR/configs/argocd/ --namespace argocd
 ARGOCD_PW=$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2)
 echo $ARGOCD_PW > /vagrant/argocd-pw
 
-echo 'prep helm'
-helm repo update
-
 echo 'install apps with argocd'
 helm upgrade applications --install $DIR/apps/ --namespace argocd
-
